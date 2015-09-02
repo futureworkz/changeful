a76db31f5692e218c86d2ae0e2f70c9d8f923add
Wayne Tng <wayne@futureworkz.com>
2015-09-02 21:57:57 +0700

    Update changeful.rb

--- lib/changeful.rb
+++ lib/changeful.rb
@@ -1,4 +1,11 @@
 require "changeful/version"
+require "changeful/models/changeful_content"
+require "changeful/helpers/view_helpers"
+require "active_support/lazy_load_hooks"
+
+ActiveSupport.on_load(:action_view) do
+    include Changeful::ViewHelpers
+end
 
 module Changeful
   # Your code goes here...
