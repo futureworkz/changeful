0fe41d03134fca57adba4365edab74b3ab2c6571
Wayne Tng <wayne@futureworkz.com>
2015-09-04 14:16:03 +0700

    Add uniqueness scope with test case

--- lib/changeful/models/changeful_content.rb
+++ lib/changeful/models/changeful_content.rb
@@ -4,7 +4,7 @@ module Changeful
   class Content < ActiveRecord::Base
     self.table_name = :changeful_contents
 
-    validates :key, :content, :view_type, presence: true
-    validates :key, uniqueness: true
+    validates :key, :content, :view_type, :file_path, presence: true
+    validates :key, uniqueness: { scope: :file_path }
   end
 end
--- spec/changeful/models/changeful_content_spec.rb
+++ spec/changeful/models/changeful_content_spec.rb
@@ -5,7 +5,7 @@ require_relative '../../../lib/changeful/models/changeful_content'
 describe Changeful::Content, type: :model do
   context ' Validation ' do
     it { is_expected.to validate_presence_of :key }
-    it { is_expected.to validate_uniqueness_of :key }
+    it { is_expected.to validate_uniqueness_of(:key).scoped_to(:file_path) }
     it { is_expected.to validate_presence_of :content }
     it { is_expected.to validate_presence_of :view_type }
   end
