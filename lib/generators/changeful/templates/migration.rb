d61de6d065aea6e121238bbad58e52f52c44aaa5
Wayne Tng <wayne@futureworkz.com>
2015-09-04 10:57:46 +0700

    Add file_path to database

--- lib/generators/changeful/templates/migration.rb
+++ lib/generators/changeful/templates/migration.rb
@@ -4,6 +4,7 @@ class CreateChangefulContents < ActiveRecord::Migration # :nodoc:
       t.string :key
       t.text :content
       t.string :view_type
+      t.string :file_path
       t.timestamps null: true
     end
   end
